f77643a612c7ad161ef62ab79e2fa03badc4c7de
enragedpixel <dmarkbroome@gmail.com>
2020-05-30 12:36:01 -0500

    Create cfgWeapons.hpp
    making custom nvgs

---
+++ 61st Aux/Addons/main/cfgWeapons.hpp
@@ -0,0 +1,11 @@
+
+class Integrated_NVG_TI_1_F;
+class NVGoggles;
+class InvisibleNVG: Integrated_NVG_TI_1_F
+  {
+		scope = 1;
+		visionMode[] = {"Normal","NVG","TI"};
+		thermalMode[] = {0,4};
+		modelOptics = "";
+    displayname = "Invisible NVG"
+	};
